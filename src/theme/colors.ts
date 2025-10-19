9c9f461fd918d08536c0f95aa06558a5013be77d
CarlaSalHua <c.salhua97@gmail.com>
2025-10-19 12:07:57 -0500

    global colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  black: '#000000',
+  black100: '#03050F',
+  blackNeutral: '#03050F',
+  white: '#FFFFFF',
+  neutral: '#FAFBFF',
+  grayNeutral: '#7981B2',
+  success: '#389E0D',
+  greenAqua: '#7DF0BA',
+  blueberry: '#4F4FFF',
+  redRimac: '#FF1C44',
+};
