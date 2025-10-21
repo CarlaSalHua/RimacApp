08e27fb96d3fb8d36ffbec512658c9faa6b3f47c
CarlaSalHua <c.salhua97@gmail.com>
2025-10-20 23:14:01 -0500

    updates

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,8 +1,10 @@
 import LoginScreen from "@/screens/LoginScreen";
+import StepsScreen from "@/screens/StepsScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
   Login: undefined;
+  StepsStatus: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -10,7 +12,8 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Login" component={LoginScreen}  />
+      <Stack.Screen name="StepsStatus" component={StepsScreen} />
     </Stack.Navigator>
   );
 }
--- src/services/PlanService.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/api/rimacApi";
-import { PlansResponse } from "@/types";
-
-export const getPlans = async (): Promise<PlansResponse> => {
-  const response = await api.get<PlansResponse>("/plans.json");
-  return response.data;
-}
